95ea648767f1d24e50aec81e920a32f934cf4c24
Haniche Zahir <zahir.haniche@inetum.com>
2024-08-22 16:42:49 +0200

    Install reactJS and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+        <h1>TEST</h1>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
