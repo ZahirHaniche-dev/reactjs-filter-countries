2757bca3a2788275b309835d9160806c9c48e635
Haniche Zahir <zahir.haniche@inetum.com>
2024-08-25 03:03:44 +0200

    Updates

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import countries from "./features/countries"
-
-export const store = configureStore({
-    reducer: {
-        countries
-    }
-})
