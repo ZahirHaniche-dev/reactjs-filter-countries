08f0cc3389a78787388a998675027456db6b68fa
Haniche Zahir <zahir.haniche@inetum.com>
2024-08-26 19:57:46 +0200

    Change link in vite config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reactjs-filter-countries/',
+  base: '/filter-countries/',
   build: {
     outDir: 'dist',
   },
